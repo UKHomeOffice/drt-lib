a45571cbde0450c4e61a101ec477f62b03d30874
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2021-11-04 09:58:47 +0000

    DRTII-480 LBA to put in alphabetic order

--- src/main/scala/uk/gov/homeoffice/drt/auth/Roles.scala
+++ src/main/scala/uk/gov/homeoffice/drt/auth/Roles.scala
@@ -6,7 +6,7 @@ import upickle.default.{readwriter, _}
 
 
 object Roles {
-  val portRoles: Set[Role] = Set(BFS, BHD, BHX, BRS, EDI, EMA, GLA, LCY, LGW, LHR, LPL, LTN, MAN, NCL, PIK, STN, LBA)
+  val portRoles: Set[Role] = Set(BFS, BHD, BHX, BRS, EDI, EMA, GLA, LBA, LCY, LGW, LHR, LPL, LTN, MAN, NCL, PIK, STN)
 
   val availableRoles: Set[Role] = Set(
     FixedPointsEdit,
