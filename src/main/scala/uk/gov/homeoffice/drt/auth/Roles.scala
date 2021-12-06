4af62ac74220d8302de50a58e0a2cc8191572529
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2021-12-06 18:21:36 +0000

    DRTII-570 adding NQY role to port roles

--- src/main/scala/uk/gov/homeoffice/drt/auth/Roles.scala
+++ src/main/scala/uk/gov/homeoffice/drt/auth/Roles.scala
@@ -6,7 +6,7 @@ import upickle.default.{readwriter, _}
 
 
 object Roles {
-  val portRoles: Set[Role] = Set(ABZ, BFS, BHD, BHX, BRS, BOH, CWL, DSA, EDI, EMA, GLA, HUY, INV, LBA, LCY, LGW, LHR, LPL, LTN, MAN, NCL, PIK, SOU, STN)
+  val portRoles: Set[Role] = Set(ABZ, BFS, BHD, BHX, BRS, BOH, CWL, DSA, EDI, EMA, GLA, HUY, INV, LBA, LCY, LGW, LHR, LPL, LTN, MAN, NCL, NQY ,PIK, SOU, STN)
 
   val availableRoles: Set[Role] = Set(
     ApiView,
