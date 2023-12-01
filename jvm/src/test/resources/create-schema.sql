824a46c46f4aa18f7d8a5fb508c57ac111d956b0
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2023-12-01 15:18:44 +0000

    DRTII-1316 test table for H2-db create schema

--- jvm/src/test/resources/create-schema.sql
+++ jvm/src/test/resources/create-schema.sql
@@ -1,13 +1,1 @@
-CREATE TABLE IF NOT EXISTS user_feedback
-(
-    email text NOT NULL,
-    actioned_at timestamp NOT NULL,
-    feedback_at timestamp,
-    close_banner boolean NULL,
-    bf_role text NULL,
-    drt_quality text NULL,
-    drt_likes text NULL,
-    drt_improvements text NULL,
-    participation_interest  boolean NULL,
-    PRIMARY KEY (email,actioned_at)
-);
+CREATE TABLE IF NOT EXISTS test
