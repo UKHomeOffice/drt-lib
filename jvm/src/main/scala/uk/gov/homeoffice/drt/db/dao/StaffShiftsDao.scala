a03da97ad134a1215fae21da336009af6dc9fb92
Nilesh Gupta <guptanilesh13@gmail.com>
2025-07-04 13:26:31 +0100

    update drt staff shift
    Signed-off-by: Nilesh Gupta <guptanilesh13@gmail.com>

--- jvm/src/main/scala/uk/gov/homeoffice/drt/db/dao/StaffShiftsDao.scala
+++ jvm/src/main/scala/uk/gov/homeoffice/drt/db/dao/StaffShiftsDao.scala
@@ -5,10 +5,13 @@ import slick.lifted.TableQuery
 import uk.gov.homeoffice.drt.db.CentralDatabase
 import uk.gov.homeoffice.drt.db.tables.{StaffShiftRow, StaffShiftsTable}
 
+import java.sql.Date
 import scala.concurrent.Future
 
 trait IStaffShiftsDao {
   def insertOrUpdate(staffShiftRow: StaffShiftRow): Future[Int]
+
+  def updateStaffShift(previousStaffShiftRow: StaffShiftRow, staffShiftRow: StaffShiftRow): Future[Int]
 
   def getStaffShiftsByPort(port: String): Future[Seq[StaffShiftRow]]
 
@@ -26,6 +29,26 @@ case class StaffShiftsDao(db: CentralDatabase) extends IStaffShiftsDao {
 
   override def insertOrUpdate(staffShiftRow: StaffShiftRow): Future[Int] =
     db.run(staffShiftsTable.insertOrUpdate(staffShiftRow))
+
+  def updateStaffShift(previousStaffShiftRow: StaffShiftRow, staffShiftRow: StaffShiftRow): Future[Int] = {
+    val deleteAction = staffShiftsTable
+      .filter(row =>
+        row.port === previousStaffShiftRow.port &&
+          row.terminal === previousStaffShiftRow.terminal &&
+          row.shiftName === previousStaffShiftRow.shiftName &&
+          row.startDate === previousStaffShiftRow.startDate &&
+          row.startTime === previousStaffShiftRow.startTime
+      ).delete
+
+    val insertAction = staffShiftsTable += staffShiftRow
+
+    if (staffShiftRow.startDate.getTime > new Date(System.currentTimeMillis()).getTime) {
+      // If the new shift is in the future, we can just insert it
+      db.run(staffShiftsTable.insertOrUpdate(previousStaffShiftRow.copy(endDate = Option(staffShiftRow.startDate))).andThen(insertAction))
+    } else {
+      db.run(deleteAction.andThen(insertAction))
+    }
+  }
 
   override def getStaffShiftsByPort(port: String): Future[Seq[StaffShiftRow]] =
     db.run(staffShiftsTable.filter(_.port === port).result)
